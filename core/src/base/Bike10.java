65d4914f959bc64ef9136d8c83d931789486cd9a
sanjay <modasiasanjay94@gmail.com>
2017-07-24 08:23:28 +0530

    Comment.

---
+++ core/src/base/Bike10.java
@@ -0,0 +1,15 @@
+package base;
+
+class Bike10{  
+	  final int speedlimit;//blank final variable  
+	    //add Comment new 
+	  
+	  Bike10(){  
+	  speedlimit=70;  
+	  System.out.println(speedlimit);  
+	  }  
+	  
+	  public static void main(String args[]){  
+	    new Bike10();  
+	 }  
+	} 
